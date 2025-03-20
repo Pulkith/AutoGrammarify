675fe308bcee13ff6198ca70be71f37acf03f83e
Pulkith <pulkith.pa@gmail.com>
2025-03-20 18:32:50 -0400

    Initial Commit

---
+++ GFy/GFyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GFyApp.swift
+//  GFy
+//
+//  Created by Pulkith Paruchuri on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct GFyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GFyTests/GFyTests.swift
@@ -0,0 +1,17 @@
+//
+//  GFyTests.swift
+//  GFyTests
+//
+//  Created by Pulkith Paruchuri on 3/20/25.
+//
+
+import Testing
+@testable import GFy
+
+struct GFyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
